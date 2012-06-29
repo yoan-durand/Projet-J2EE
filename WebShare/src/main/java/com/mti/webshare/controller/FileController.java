3c82111ee2a73e4eccb14e9df2b438883a3e9d3a
comby-v <vincent.comby@gmail.com>
2012-06-29 18:42:52 +0200

    fix name FileItem

--- WebShare/src/main/java/com/mti/webshare/controller/FileController.java
+++ WebShare/src/main/java/com/mti/webshare/controller/FileController.java
@@ -17,14 +17,14 @@ package com.mti.webshare.controller;
 
 import com.mti.webshare.dao.FileDAO;
 import com.mti.webshare.model.File;
-import com.mti.webshare.model.FileItem;
+import com.mti.webshare.model.FileView;
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -40,12 +40,12 @@ public class FileController {
             throws ServletException, IOException {
         
             List<File> list_file_bdd = fileDAO.getList();
-            List<FileItem> list_file_item = new ArrayList<FileItem> ();
+            List<FileView> list_file_item = new ArrayList<FileView> ();
             
             for(Iterator<File> it = list_file_bdd.iterator (); it.hasNext ();)
             {
-                  FileItem file = new FileItem ();
-                   file.convert(it.next());
+                  FileView file = new FileView ();
+                  file.convert((File)it.next());
                   list_file_item.add(file);
             }
             
