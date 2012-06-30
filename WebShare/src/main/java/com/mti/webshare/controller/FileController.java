ed6448601fac17a4b4c66e013692ec90aacc5ac9
Mika <vincent.comby@gmail.com>
2012-06-30 10:10:56 +0200

    remove comments top of FileController.java

--- WebShare/src/main/java/com/mti/webshare/controller/FileController.java
+++ WebShare/src/main/java/com/mti/webshare/controller/FileController.java
@@ -1,18 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author vince
- */
-
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mti.webshare.controller;
 
 import com.mti.webshare.dao.FileDAO;
