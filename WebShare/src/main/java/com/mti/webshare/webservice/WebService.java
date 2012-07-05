753ff5626dfc3f99778b056f2432d495c1fa83b2
Mika <vincent.comby@gmail.com>
2012-07-05 15:14:09 +0200

    web services working

--- WebShare/src/main/java/com/mti/webshare/webservice/WebService.java
+++ WebShare/src/main/java/com/mti/webshare/webservice/WebService.java
@@ -4,6 +4,11 @@
  */
 package com.mti.webshare.webservice;
 
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 /**
  *
  * @author damiensoulard
@@ -13,11 +18,16 @@ package com.mti.webshare.webservice;
   endpointInterface = "com.mti.webshare.webservice.IWebService",
   serviceName = "WebService")
 public class WebService implements IWebService{
-
-    @Override
-    public int add(int a, int b) {
-        return a+b;
+    
+    public WebService()
+    {
     }
     
-    
+    @GET
+    @Path("/add/{a}/{b}")
+    @Produces("text/plain")
+    @Override
+    public int add(@PathParam("a")int a, @PathParam("b")int b) {
+        return a+b;
+    }
 }
