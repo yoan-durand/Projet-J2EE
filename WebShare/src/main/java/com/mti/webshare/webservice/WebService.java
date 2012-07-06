d7be07fc2f1011f09b86fbb93934a7c2256baa7a
iDams <damien.soulard@gmail.com>
2012-07-06 18:25:21 +0200

    webservice getUserList

--- WebShare/src/main/java/com/mti/webshare/webservice/WebService.java
+++ WebShare/src/main/java/com/mti/webshare/webservice/WebService.java
@@ -8,6 +8,22 @@ import com.mti.webshare.dao.FileDAO;
 import com.mti.webshare.dao.UserDAO;
 import com.mti.webshare.model.FileUploaded;
 import com.mti.webshare.model.User;
+import java.io.StringReader;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.mail.internet.ContentDisposition;
+import javax.mail.internet.ParseException;
+import javax.xml.namespace.QName;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.ws.Dispatch;
+import javax.xml.ws.Service;
+import javax.xml.ws.handler.MessageContext;
+import javax.xml.ws.http.HTTPBinding;
+import org.apache.cxf.jaxrs.client.WebClient;
+import org.codehaus.jettison.json.JSONArray;
 import org.springframework.beans.factory.annotation.Autowired;
 /**
  *
@@ -62,5 +78,14 @@ public class WebService implements IWebService{
             return "File not found";
         }
         return fileDAO.toJson(file);
+    }
+    
+    @Override
+    public String getUserList(){
+        List<User> u = userDAO.getList();
+        if (u == null){
+            return "No users found";
+        }
+        return userDAO.toJson(u);
     }
 }
